064a99c1e9ae546c3c3e64a9f1de003f976a8b8b
Maksym Mukanovskyi <maksymmukanovskyi@gmail.com>
2020-05-10 12:33:03 -0600

    adjust webpack, folder setup, cleaned css and html

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+'use strict'
+import '../styles/styles.css'
+console.log('hello world');
+// const data = {
+//     "artist": {
+//         "name": "Cher",
+//         "mbid": "bfcc6d75-a6a5-4bc6-8282-47aec8531818",
+//         "url": "https://www.last.fm/music/Cher",
+//         "image": [
+//             {
+//                 "#text": "https://lastfm-img2.akamaized.net/i/u/34s/345a7b45ef442c8340f9b19263021c68.png",
+//                 "size": "small"
+//             }]
+//         }};
+
+//         'use strict';
+// const contentArtist = document.querySelector('#artist');
+// const contentAlbum = document.querySelector('#albums');
+// const content = document.querySelector('.content');
+// const chart = document.querySelectorAll('.chart-list');
+// const album = document.querySelector('.content__album');
+// const divChart = document.querySelector('.chart');
+// const contentSongs = document.querySelector('#songs');
---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    // stats:  'verbose',
+    stats: 'errors-warnings',
+    context: path.resolve(__dirname, './src'),
+    mode: 'development',
+    entry: {
+        main: ['@babel/polyfill', './js/index.js'],
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.js', '.json', '.png'],
+        alias: {
+            '@models': path.resolve(__dirname, './src/js'),
+            '@': path.resolve(__dirname, './src'),
+
+        }
+    },
+    devServer: {
+        contentBase: './dist',
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin([
+            {
+                from: path.resolve(__dirname, './src/assets/favicon.png'),
+                to:path.resolve(__dirname, 'dist'), 
+            },
+            // {
+            //     from: path.resolve(__dirname, './src/assets/sprite.svg'),
+            //     to:path.resolve(__dirname, 'dist'),
+            //   }
+        ]),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        }),
+        
+    ],
+
+    
+    module: {
+        rules: [
+            { 
+                test: /\.js$/, 
+                exclude: /node_modules/, 
+                loader: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            '@babel/preset-env'
+                        ],
+                        plugins: [
+                            '@babel/plugin-proposal-class-properties'
+                        ]
+                    }
+                } 
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/i,
+                use: ['file-loader'],
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+            }
+        ]
+    }
+}
