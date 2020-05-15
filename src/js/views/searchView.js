84b350066fd8fd8fb4fa6d742b4aca7fa4fea3ad
Maksym Mukanovskyi <maksymmukanovskyi@gmail.com>
2020-05-14 18:29:50 -0600

    added media rules in css

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -5,15 +5,17 @@ export const clearResults = () => elements.searchContent.innerHTML = '';
 
 const renderMusicSearch = music => {
     const markup = `
+    <li class="card-container">
         <a class="artist-card" href="#${music.id}">
             <figure>
                     <img src="${music.album.cover_medium}" alt="artist" class="artist-card__img">
                 <figcaption>
-                    <p class="artist-card__name">${music.artist.name}</p>
-                    <p class="artist-card__number-albums">${music.title}</p>
+                    <p class="artist-card__name">${music.title}</p>
+                    <p class="artist-card__number-albums">${music.artist.name}</p>
                 </figcaption>
             </figure>
         </a>
+    </li>
     `;
     elements.searchContent.insertAdjacentHTML('beforeend', markup);
 
