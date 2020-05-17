afbb9834b1373d5530c236396dc0ed38353ccbfa
Maksym Mukanovskyi <maksymmukanovskyi@gmail.com>
2020-05-16 21:53:08 -0600

    added loader

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@
 import '../styles/styles.css'
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import {elements} from './views/base';
+import {elements, renderLoader, clearLoader, elementString} from './views/base';
 
 let albumMethods = 'https://api.deezer.com/album/103248';
 let chatsMethod = 'https://api.deezer.com/chart/';
@@ -18,13 +18,16 @@ const controlSearch = async () => {
         state.search = new Search(query);
         searchView.clearInput();
         searchView.clearResults();
+        renderLoader(elements.mainContainer)
 
         try{
             await state.search.getResults();
             searchView.renderResults(state.search.musicSearch.data.data)
             console.log(state.search.musicSearch.data.data)
+            clearLoader();
         }catch(error){
             alert('Something wrong with search....')
+            clearLoader();
         }
 
     }
@@ -32,6 +35,8 @@ const controlSearch = async () => {
 
 elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
+    
+
     controlSearch();
 })
 
