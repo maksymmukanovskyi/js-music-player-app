a828585dea55d8b3c7a714a151de08ed48b43533
Maksym Mukanovskyi <maksymmukanovskyi@gmail.com>
2020-07-05 16:12:07 -0600

    fixing bug with  vollume button

--- src/js/views/playerView.js
+++ src/js/views/playerView.js
@@ -40,7 +40,7 @@ export const updateProgress = () => {
 }
 
 export const updateVolume = () => {
-    elements.volumeProgress.style.height = player.volume * 100 + '%';
+    elements.volumeProgress.style.height = elements.player.volume * 100 + '%';
   if(elements.player.volume >= 0.5) {
     elements.speaker.attributes.d.value = 'M14.667 0v2.747c3.853 1.146 6.666 4.72 6.666 8.946 0 4.227-2.813 7.787-6.666 8.934v2.76C20 22.173 24 17.4 24 11.693 24 5.987 20 1.213 14.667 0zM18 11.693c0-2.36-1.333-4.386-3.333-5.373v10.707c2-.947 3.333-2.987 3.333-5.334zm-18-4v8h5.333L12 22.36V1.027L5.333 7.693H0z';  
   } else if(elements.player.volume < 0.5 && elements.player.volume > 0.05) {
